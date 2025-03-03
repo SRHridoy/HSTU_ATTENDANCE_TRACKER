4a16ff933f77dda47976516b039194a124dedc3a
Khaled-Amin-Shawon <mdkhaledaminshawon@gmail.com>
2025-03-04 02:51:37 +0600

    Refactor CourseDBHelper to rename course credit column and update related methods

--- lib/services/db_services/course_db_helper.dart
+++ lib/services/db_services/course_db_helper.dart
@@ -15,9 +15,9 @@ class CourseDBHelper {
   static final String COLUMN_COURSE_SNO = 'course_sno';
   static final String COLUMN_COUSE_CODE = 'course_code';
   static final String COLUMN_COUSE_NAME = 'course_name';
-  static final String COLUMN_BATCH_NAME = 'batch_name';
+  static final String COLUMN_BATCH_NAME = 'batch_no';
   static final String COLUMN_SESSION = 'session';
-  static final String COLUMN_COURSE_CREDIT = 'course_credit'; // Add this line
+  static final String COLUMN_CREDIT = "credit";
 
   Database? myDb;
 
@@ -37,75 +37,68 @@ class CourseDBHelper {
         $COLUMN_COUSE_CODE TEXT,
         $COLUMN_COUSE_NAME TEXT,
         $COLUMN_BATCH_NAME TEXT,
-        $COLUMN_SESSION TEXT,
-        $COLUMN_COURSE_CREDIT INTEGER // Add this line
+        $COLUMN_SESSION   TEXT,
+        $COLUMN_CREDIT  INTEGER
       );
       ''');
     }, version: 1);
   }
 
-
   Future<bool> addCourse(
       {required String courseCode,
-        required String courseName,
-        required String batchName,
-        required String session,
-        required int courseCredit, // Add this parameter
-      }) async{
-
+      required String courseName,
+      required String batchName,
+      required String session,
+      required int credit
+      }) async {
     var db = await getDB();
 
     int rowsEffected = await db.insert(TABLE_COURSE, {
-      COLUMN_COUSE_CODE:courseCode,
-      COLUMN_COUSE_NAME:courseName,
-      COLUMN_BATCH_NAME:batchName,
-      COLUMN_SESSION:session,
-      COLUMN_COURSE_CREDIT: courseCredit, // Add this line
+      COLUMN_COUSE_CODE: courseCode,
+      COLUMN_COUSE_NAME: courseName,
+      COLUMN_BATCH_NAME: batchName,
+      COLUMN_SESSION: session,
+      COLUMN_CREDIT: credit
     });
 
-    return rowsEffected>0;
+    return rowsEffected > 0;
   }
 
-
-  Future<List<Map<String,dynamic>>> getAllCourse() async{
+  Future<List<Map<String, dynamic>>> getAllCourse() async {
     var db = await getDB();
 
-    List<Map<String,dynamic>> courses = await db.query(TABLE_COURSE);
+    List<Map<String, dynamic>> courses = await db.query(TABLE_COURSE);
     return courses;
   }
 
   Future<bool> updateCourse(
       {required String courseCode,
-        required String courseName,
-        required String batchName,
-        required String session,
-        required int sno
-      }) async{
-
+      required String courseName,
+      required String batchName,
+      required String session,
+      required int credit,
+      required int sno}) async {
     var db = await getDB();
 
-    int rowsEffected = await db.update(TABLE_COURSE, {
-      COLUMN_COUSE_CODE:courseCode,
-      COLUMN_COUSE_NAME:courseName,
-      COLUMN_BATCH_NAME:batchName,
-      COLUMN_SESSION:session
-    },where: "$COLUMN_COURSE_SNO = $sno");
+    int rowsEffected = await db.update(
+        TABLE_COURSE,
+        {
+          COLUMN_COUSE_CODE: courseCode,
+          COLUMN_COUSE_NAME: courseName,
+          COLUMN_BATCH_NAME: batchName,
+          COLUMN_SESSION: session,
+          COLUMN_CREDIT: credit
+        },
+        where: "$COLUMN_COURSE_SNO = $sno");
 
-    return rowsEffected>0;
-  }
-
-  Future<bool> deleteCourse({required int sno})async{
-    var db = await getDB();
-
-    int rowsEffected = await db.delete(TABLE_COURSE,where: "$COLUMN_COURSE_SNO = ?",whereArgs: ["$sno"]);
     return rowsEffected > 0;
   }
 
+  Future<bool> deleteCourse({required int sno}) async {
+    var db = await getDB();
+
+    int rowsEffected = await db.delete(TABLE_COURSE,
+        where: "$COLUMN_COURSE_SNO = ?", whereArgs: ["$sno"]);
+    return rowsEffected > 0;
+  }
 }
-
-
-
-
-
-
-
